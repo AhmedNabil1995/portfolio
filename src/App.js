eb22e4a66677e03adc8b5e8500a19315e283d0e2
AhmedNabil1995 <ahmed.nabil55586@gmail.com>
2022-09-01 22:46:26 +0200

    create home and portofolio pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Menu from './components/menu/Menu';
 import Home from './pages/home/Home';
 import Portfolio from './pages/portfolio/Portfolio';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Working from './components/working/Working';
 
 function App() {
+  let [display,setDispaly] = useState(true);
+
   return (
     <div className="App">
       <BrowserRouter>
       <Menu />
+      {display&&<Working setDispaly={setDispaly}/>}
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/portfolio' element={<Portfolio />}/>
